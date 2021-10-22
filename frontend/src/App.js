11e5d9058884a7212fa279b8b0c90dba633179d1
Zak Warsame <zakwarsamee@gmail.com>
2021-10-22 23:07:45 +0000

    add .env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import useApplicationData from './hooks/useApplicationData';
 
@@ -27,13 +26,12 @@ import useApplicationData from './hooks/useApplicationData';
 const App = () => {
   const {
       state,
-      dispatch
+      // dispatch
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
 return (<div className="App" >
-  <h1> Users </h1>
-
+  <h1> Users!! </h1>
   <ul> {userList} </ul>
 </div >
 );
