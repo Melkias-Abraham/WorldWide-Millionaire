91bc52efc57e4e6fcd084d41e2c7129bad85c85b
96sMicks <melkiasae@hotmail.com>
2021-10-23 19:59:57 +0000

    Seeds for all trivia questions and answers

--- backend/db/seeds/02_question_seeds.sql
+++ backend/db/seeds/02_question_seeds.sql
@@ -122,12 +122,248 @@ VALUES
     'hard'
   ),
    (
-    -- 20
+    -- 21
     'Europe',
-    'In which country would you be in if you were flying over the pre-Colombian Nazca lines?',
+    'Which country is home to the Eifel Tower?',
+    'easy'
+  ),
+  (
+    -- 22
+    'Europe',
+    'Which part of Europe is Scandinavia part of?',
+    'easy'
+  ),
+  (
+    -- 23
+    'Europe',
+    'Which country was divided into East and West between the 1940s and 1990s?',
+    'easy'
+  ),
+  (
+    -- 24
+    'Europe',
+    'Which is the most populous capital city in Europe?',
+    'easy'
+  ),
+  (
+    -- 25
+    'Europe',
+    'What is the most common spoken language in Europe?',
+    'medium'
+  ),
+  (
+    -- 26
+    'Europe',
+    'Which of these countries joined the European Union most recently?',
+    'medium'
+  ),
+  (
+    -- 27
+    'Europe',
+    'Which of these countries does not have a monarchy?',
+    'medium'
+  ),
+  (
+    -- 28
+    'Europe',
+    'Which country has won the most Eurovision titles?',
     'hard'
   ),
+   (
+    -- 29
+    'Europe',
+    'In which of these countries does the Danube not flow?',
+    'hard'
+  ),
+   (
+    -- 30
+    'Europe',
+    "How long is Iceland's Ring Road?",
+    'hard'
+  ),
+   (
+    -- 31
+    'Asia',
+    "Which is Asia's largest country?",
+    'easy'
+  ),
+  (
+    -- 32
+    'Asia',
+    "What is the name of the highest mountain in Asia?",
+    'easy'
+  ),
+   (
+    -- 33
+    'Asia',
+    "Which country is known as a Country of a Thousand Islands?",
+    'easy'
+  ),
+   (
+    -- 34
+    'Asia',
+    "How many people live in Asia?",
+    'easy'
+  ),
+   (
+    -- 35
+    'Asia',
+    "How many countries are in Asia?",
+    'medium'
+  ),
+   (
+    -- 36
+    'Asia',
+    "Which of these cities lies on the Saigon River?",
+    'medium'
+  ),
+    (
+    -- 37
+    'Asia',
+    "In what country is Sichuan found?",
+    'medium'
+  ),
+   (
+    -- 38
+    'Asia',
+    "What separates China from India?",
+    'hard'
+  ),
+    (
+    -- 39
+    'Asia',
+    "Which city hosted the 1988 Olympic Games?",
+    'hard'
+  ),
+    (
+    -- 40
+    'Asia',
+    "A 25km causeway links Saudi Arabia with which other country?",
+    'hard'
+  ),
+   (
+    -- 41
+    'Africa',
+    "Which of these is the smallest county in Africa by area?",
+    'easy'
+  ),
+   (
+    -- 42
+    'Africa',
+    "Which of the following is not in Africa?",
+    'easy'
+  ),
+   (
+    -- 43
+    'Africa',
+    "Africa makes up ____ percent of the world’s total land area.",
+    'easy'
+  ),
+   (
+    -- 44
+    'Africa',
+    "Which of these is the largest African city?",
+    'easy'
+  ),
+   (
+    -- 45
+    'Africa',
+    "Which country is based around the hit Disney Movie of animals trying to go back home in the wild?",
+    'medium'
+  ),
+  (
+    -- 46
+    'Africa',
+    "The Mercato is one of Africa’s largest open-air markets and is located in the city of Addis Ababa. But in which country is it?",
+    'medium'
+  ),
+  (
+    -- 47
+    'Africa',
+    "How many countries does the Nile River flow through?",
+    'medium'
+  ),
+  (
+    -- 48
+    'Africa',
+    "What is Africa's newest country?",
+    'hard'
+  ),
+  (
+    -- 49
+    'Africa',
+    "Pick the current president of an African country? (As of 2021)",
+    'hard'
+  ),
+  (
+    -- 50
+    'Africa',
+    "Which country was called Upper Volta until 1984?",
+    'hard'
+  ),
+  (
+    -- 51
+    'Australia',
+    "What is the constellation on Australia's flag?",
+    'easy'
+  ),
+  (
+    -- 52
+    'Australia',
+    "What is Austrailia national animal?",
+    'easy'
+  ),
+  (
+    -- 53
+    'Australia',
+    "What is the only living structure that can be seen from space?",
+    'easy'
+  ),
+  (
+    -- 54
+    'Australia',
+    "How many states are there in Australia?",
+    'easy'
+  ),
+  (
+    -- 55
+    'Australia',
+    "How many of the world’s top 25 deadliest snakes live in Australia?",
+    'medium'
+  ),
+  (
+    -- 56
+    'Australia',
+    "What 2 native Australian mammals lay eggs?",
+    'medium'
+  ),
+  (
+    -- 57
+    'Australia',
+    "Australia is home to the longest fence in the world. What is it called?",
+    'medium'
+  ),
+  (
+    -- 58
+    'Australia',
+    "How many countries are bigger than Australia by area?",
+    'hard'
+  ),
+  (
+    -- 59
+    'Australia',
+    "How many times did Australia win the Rugby World Cup?",
+    'hard'
+  ),
+  (
+    -- 60
+    'Australia',
+    "What do you call the Aussie favourite sponge cake cubes dipped in chocolate and rolled in coconut?",
+    'hard'
+  ),
+
   
+
   
 
 
