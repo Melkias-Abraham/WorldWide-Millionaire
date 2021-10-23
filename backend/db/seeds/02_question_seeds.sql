e959f677794d1bfd24e259ce4ad70a5a3d290746
96sMicks <melkiasae@hotmail.com>
2021-10-23 00:51:48 +0000

    Compeleted Q&A for North and South America

---
+++ backend/db/seeds/02_question_seeds.sql
@@ -0,0 +1,135 @@
+INSERT INTO
+questions (region_id, question, difficulty)
+VALUES
+  (
+    -- 1
+    'North America',
+    'What is the capital city of Canada?',
+    'easy'
+  ),
+  (
+    -- 2
+    'North America',
+    'Alaska is part of what country?',
+    'easy'
+  ),
+  (
+    -- 3
+    'North America',
+    'Which is the largest country in North America? (by size)',
+    'easy'
+  ),
+  ( 
+    -- 4
+    'North America',
+    'What is the national animal of Canada? ',
+    'easy'
+  ),
+  ( 
+    -- 5
+    'North America',
+    'How many countries are part of North America?',
+    'medium'
+  ),
+  (
+    -- 6
+    'North America',
+    'What is the largest city in North America?',
+    'medium'
+  ),
+  (
+    -- 7
+    'North America',
+    'The United States of America, Canada and Mexico all share what colours on their flags?',
+    'medium'
+  ),
+  (
+    -- 8
+    'North America',
+    'Which is the highest Mountain in North America?',
+    'hard'
+  ),
+  (
+    -- 9
+    'North America',
+    'Which of these are the tallest waterfall in North America?',
+    'hard'
+  ),
+  (
+    -- 10
+    'North America',
+    'What are the cordilleras?',
+    'hard'
+  ),
+  (
+    -- 11
+    'South America',
+    'Which is the biggest country in South America? (by size)',
+    'easy'
+  ),
+   (
+    -- 12
+    'South America',
+    'Which is the smallest country in South America? (by size)',
+    'easy'
+  ),
+   (
+    -- 13
+    'South America',
+    'What is the longest river in South America?',
+    'easy'
+  ),
+   (
+    -- 14
+    'South America',
+    'Which one of these countries is a landlocked country?',
+    'easy'
+  ),
+   (
+    -- 15
+    'South America',
+    'Which South America country has both a Pacific and Atlantic coast?',
+    'medium'
+  ),
+   (
+    -- 16
+    'South America',
+    'The most populous city in South America?',
+    'medium'
+  ),
+   (
+    -- 17
+    'South America',
+    'Which South American country is located in the Northern Hemisphere?',
+    'medium'
+  ),
+   (
+    -- 18
+    'South America',
+    'Which of these cities has never been capital of Brazil?',
+    'hard'
+  ),
+   (
+    -- 19
+    'South America',
+    'What is an arepa?',
+    'hard'
+  ),
+  (
+    -- 20
+    'South America',
+    'In which country would you be in if you were flying over the pre-Colombian Nazca lines?',
+    'hard'
+  ),
+   (
+    -- 20
+    'Europe',
+    'In which country would you be in if you were flying over the pre-Colombian Nazca lines?',
+    'hard'
+  ),
+  
+  
+
+
+
+
---
+++ backend/db/seeds/03_answers_seeds.sql
@@ -0,0 +1,413 @@
+INSERT INTO
+answers (question_id, answer, correct)
+VALUES
+  (
+    1,
+    'Washington DC',
+    false
+
+  ), 
+  (
+    1,
+    'Ottawa',
+    true
+  ),
+  (
+    1,
+    'Toronto',
+    false
+
+  ),
+  (
+    1,
+    'New York City',
+    false
+  ), 
+  (
+    2,
+    'United States of America',
+    true
+  ),
+  (
+    2,
+    'Canada',
+    false
+
+  ),
+  (
+    2,
+    'Artic Circle',
+    false
+
+  ),
+  (
+    2,
+    'Greenland',
+    false
+  ),
+  (
+    3,
+    'United States of America',
+    false
+  ),
+  (
+    3,
+    'Mexico',
+    false
+  ),
+  (
+    3,
+    'Canada',
+    true
+  ),
+  (
+    3,
+    'Greenland',
+    false
+  ),
+  (
+    4,
+    'The Beaver',
+    true
+  ),(
+    4,
+    'The Moose',
+    false
+  ),(
+    4,
+    'The Bear',
+    false
+  ),(
+    4,
+    'The Wolf',
+    false
+  ),
+  (
+    5,
+    '27',
+    false
+  ),
+   (
+    5,
+    '4',
+    false
+  ),
+   (
+    5,
+    '16',
+    false
+  ),
+   (
+    5,
+    '23',
+    true
+  ),
+  (
+    6,
+    'New York City',
+    false
+  ),
+  (
+    6,
+    'Los Angeles',
+    false
+  ),
+  (
+    6,
+    'Toronto',
+    false
+  ),
+  (
+    6,
+    'Mexico City',
+    true
+  ),
+   (
+    7,
+    'Red & White',
+    true
+  ),
+   (
+    7,
+    'White',
+    false
+  ),
+   (
+    7,
+    'Red & Blue',
+    false
+  ),
+   (
+    7,
+    'Blue & White',
+    false
+  ),
+  (
+    8,
+    'Mount Whitney',
+    false
+  ),
+  (
+    8,
+    'Mount Logan',
+    false
+  ),
+  (
+    8,
+    'Denali',
+    true
+  ),
+  (
+    8,
+    'Pico do Orizaba',
+    false
+  ),
+   (
+    9,
+    'Della Falls',
+    false
+  ),
+   (
+    9,
+    'Victoria Falls',
+    false
+  ),
+   (
+    9,
+    'Niagara Falls',
+    false
+  ),
+  (
+    9,
+    'Yosemite Falls',
+    true
+  ),
+ (
+    10,
+    'A large set of lakes',
+    false
+  ),
+   (
+    10,
+    'A mountain range',
+    true
+  ),
+   (
+    10,
+    'A group of cities',
+    false
+  ),
+   (
+    10,
+    'A set of active geysers',
+    false
+  ),
+   (
+    11,
+    'Argentina',
+    false
+  ),
+    (
+    11,
+    'Brazil',
+    true
+  ),
+    (
+    11,
+    'Chile',
+    false
+  ),
+    (
+    11,
+    'Columbia',
+    false
+  ),
+   (
+    12,
+    'Equador',
+    false
+  ),
+   (
+    12,
+    'Guyana',
+    false
+  ),
+ (
+    12,
+    'Suriname',
+    true
+  ),
+  (
+    12,
+    'Uruguay',
+    false
+  ),
+ (
+    13,
+    'Amazon River',
+    true
+  ),
+  (
+    13,
+    'Parana River',
+    false
+  ),
+  (
+    13,
+    'Purus River',
+    false
+  ),
+  (
+    13,
+    'Jurua River',
+    false
+  ),
+  (
+    14,
+    'Paraguay',
+    true
+  ),(
+    14,
+    'Equador',
+    false
+  ),(
+    14,
+    'Guyana',
+    false
+  ),(
+    14,
+    'Uruguay',
+    false
+  ),
+  (
+    15,
+    'Argentina',
+    false
+  ),(
+    15,
+    'Peru',
+    false
+  ),
+(
+    15,
+    'Brazil',
+    false
+  ),
+  (
+    15,
+    'Columbia',
+    true
+  ),
+  (
+    16,
+    'Buenos Aires',
+    false
+  ),
+  (
+    16,
+    'Lima',
+    false
+  ),
+  (
+    16,
+    'Sao Paolo',
+    true
+  ),
+  (
+    16,
+    'Rio de Janerio',
+    false
+  ),
+  (
+    17,
+    'Bolivia',
+    false
+  ),
+  (
+    17,
+    'Venezuela',
+    true
+  ),
+  (
+    17,
+    'Argentina',
+    false
+  ),
+  (
+    17,
+    'Uruguay',
+    false
+  ),
+  (
+    18,
+    'Salvador',
+    false
+  ),
+  (
+    18,
+    'Rio de Janeiro',
+    false
+  ),
+  (
+    18,
+    'Brasilia',
+    false
+  ),
+  (
+    18,
+    'Sao Paulo',
+    true
+  ),
+   (
+    19,
+    'A corn pancake popular in Venezuela and Colombia',
+    true
+  ),
+   (
+    19,
+    'A Colombian delicacy of cheese dipped in hot chocolate',
+    false
+  ),
+   (
+    19,
+    'A cheese-filled omelette from the Ecuadorian highlands',
+    false
+  ),
+   (
+    19,
+    'A fish found off the coast of Argentina and Uruguay',
+    false
+  ),
+   (
+    20,
+    'Brazil',
+    false
+  ),
+   (
+    20,
+    'Uruguay',
+    false
+  ),
+   (
+    20,
+    'Chile',
+    false
+  ),
+   (
+    20,
+    'Peru',
+    true
+  ),
+
+
+
+
+
+
+
+
+
+
+
+
+
