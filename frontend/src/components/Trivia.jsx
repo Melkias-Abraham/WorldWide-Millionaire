ad0e2febbc965178403c7188859c89b2dd323f82
96sMicks <melkiasae@hotmail.com>
2021-10-25 20:19:05 +0000

    Add Trivia component

---
+++ frontend/src/components/Trivia.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './Trivia.css'
+
+export default function Trivia(props) {
+  console.log(props)
+  const { onStart, questionNumber, continent, state } = props;
+  const [question, setQuestion] = useState(null)
+
+  useEffect(() => {
+    onStart(continent.id)
+  }, [continent]);
+
+  useEffect(() => {
+    if (!state.questions) return <span>loading...</span>;
+    setQuestion(state.questions[questionNumber-1])
+  }, [state.questions, questionNumber]);
+
+  return (
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {/* <div className="answer correct">Ottawa</div>
+        <div className="answer wrong ">New York City</div>
+        <div className="answer wrong">Toronto</div>
+        <div className="answer wrong">Washington D.C</div> */}
+      </div>
+    </div>
+  )
+};
