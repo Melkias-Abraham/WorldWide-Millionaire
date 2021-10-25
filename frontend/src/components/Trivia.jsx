d034d883a52b37726fec10eb09fd0041ba790d36
96sMicks <melkiasae@hotmail.com>
2021-10-25 20:26:03 +0000

    Able to know fetch questions and answers now implementing selected Answer logic

--- frontend/src/components/Trivia.jsx
+++ frontend/src/components/Trivia.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import './Trivia.css'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./Trivia.css";
 
 export default function Trivia(props) {
-  console.log(props)
+  console.log(props);
   const { onStart, questionNumber, continent, state } = props;
-  const [question, setQuestion] = useState(null)
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setselectedAnswer] = useState(null)
 
   useEffect(() => {
-    onStart(continent.id)
+    onStart(continent.id);
   }, [continent]);
 
   useEffect(() => {
     if (!state.questions) return <span>loading...</span>;
-    setQuestion(state.questions[questionNumber-1])
+    setQuestion(state.questions[questionNumber - 1]);
   }, [state.questions, questionNumber]);
 
   return (
     <div className="trivia">
       <div className="question">{question?.question}</div>
       <div className="answers">
+        {question?.answers[0].map((answer) => (
+          <div className="answer" onClick={handleClick}>{answer.answer}</div>
+        ))}
         {/* <div className="answer correct">Ottawa</div>
         <div className="answer wrong ">New York City</div>
         <div className="answer wrong">Toronto</div>
         <div className="answer wrong">Washington D.C</div> */}
       </div>
     </div>
-  )
-};
+  );
+}
