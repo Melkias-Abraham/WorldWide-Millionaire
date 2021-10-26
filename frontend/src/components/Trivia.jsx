1a3a85e22910630549c25a07241882175ac6add1
Wahab Alamin <wahabalamin@gmail.com>
2021-10-26 12:45:21 -0400

    add display to to game page

--- frontend/src/components/Trivia.jsx
+++ frontend/src/components/Trivia.jsx
@@ -4,12 +4,18 @@ import "./Trivia.css";
 
 
 export default function Trivia(props) {
- 
+
   const { onStart, questionNumber, continent, state, setQuestionNumber } = props;
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setselectedAnswer] = useState(null)
   const [className, setClassName] = useState("answer")
+  const [remainingTime, setRemainingTime] = useState(30)
 
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prev) => prev - 1)
+    }, 1000)
+  }, [])
 
   useEffect(() => {
     onStart(continent.id);
@@ -32,25 +38,61 @@ export default function Trivia(props) {
 
     delay(3000, () => {
       setClassName(ans.correct ? "answer correct" : "answer wrong ")
-    }); 
+    });
 
     delay(6000, () => {
       if (ans.correct) {
-        setQuestionNumber((prev) => prev +1)
+        setQuestionNumber((prev) => prev + 1)
       }
     })
   }
 
+  const moneyAmounts = [
+    { id: 10, amount: "$ 1000000" },
+    { id: 9, amount: "$ 500000" },
+    { id: 8, amount: "$ 250000" },
+    { id: 7, amount: "$ 100000" },
+    { id: 6, amount: "$ 50000" },
+    { id: 5, amount: "$ 10000" },
+    { id: 4, amount: "$ 5000" },
+    { id: 3, amount: "$ 1000" },
+    { id: 2, amount: "$ 500" },
+    { id: 1, amount: "$ 100" }
+  ];
+
   return (
     <div className="trivia">
-      <div className="question">{question?.question}</div>
-      <div className="answers">
-        {question?.answers[0].map((answer) => (
-          <div className={selectedAnswer === answer ? className : "answer"} onClick={() => handleClick(answer)}>
-            {answer.answer}
+      <div className="main">
+
+
+        <div className="top">
+          <div className={remainingTime < 11 ? "timer runningout" : "timer"}>{remainingTime}</div>
+        </div>
+
+        <div className="bottom">
+          <div className="question">{question?.question}</div>
+
+          <div className="answers">
+            {question?.answers[0].map((answer) => (
+              <div className={selectedAnswer === answer ? className : "answer"} onClick={() => handleClick(answer)}>
+                {answer.answer}
+              </div>
+            ))}
           </div>
-        ))}
+
+        </div>
       </div>
+
+      <div className="money">
+        <ul className="moneyList">
+          {moneyAmounts.map((level) => (
+            <li key={level.id} className={level.id === questionNumber ? "moneyListItem current" : "moneyListItem"}>
+              {level.amount}
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
