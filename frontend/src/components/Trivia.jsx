e46c9511fdeceb5300c565ac3c0e13dc12875df0
96sMicks <melkiasae@hotmail.com>
2021-10-27 21:07:28 +0000

    A pause function to stop the timer once a answer gets clicked

--- frontend/src/components/Trivia.jsx
+++ frontend/src/components/Trivia.jsx
@@ -10,19 +10,23 @@ export default function Trivia(props) {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setselectedAnswer] = useState(null)
   const [className, setClassName] = useState("answer")
-  const [remainingTime, setRemainingTime] = useState(30)
+  const [remainingTime, setRemainingTime] = useState(5)
+  const [pause, setPause] = useState(false)
+  
 
 
   useEffect(() => {
     
-    const interval = setInterval(() => {
+    if (!pause) { 
+      const interval = setInterval(() => {
       setRemainingTime((prev) => prev - 1)
     }, 1000)
-
+  
     if(remainingTime === 0 ) {
       return setStop(true);
     }
    return () => clearInterval(interval)
+  } 
   }, [setStop, remainingTime])
 
   useEffect(() => {
@@ -47,6 +51,7 @@ export default function Trivia(props) {
   const handleClick = (ans) => {
     setselectedAnswer(ans);
     setClassName("answer active");
+    setPause(true)
 
     delay(3000, () => {
       setClassName(ans.correct ? "answer correct" : "answer wrong ")
@@ -56,10 +61,10 @@ export default function Trivia(props) {
       if (ans.correct) {
         setQuestionNumber((prev) => prev + 1)
         setselectedAnswer(null)
+        setPause(false)
         setRemainingTime(30)
       } else {
         setStop(true)
-        
       }
     })
   }
