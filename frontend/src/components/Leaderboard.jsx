ee83613631cc32ef92a148f9da1f930aec93cb73
Zak Warsame <zakwarsamee@gmail.com>
2021-11-01 20:46:46 +0000

    add a loading bar

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext} from "react";
+import React, { useEffect, useContext } from "react";
 import "./Leaderboard.css";
 
 import { styled } from "@mui/material/styles";
@@ -11,19 +11,19 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import { stateContext } from "../providers/StateProvider";
-import ScoreNav from './ScoreNav';
-
+import ScoreNav from "./ScoreNav";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: '#5D3FD3',
+    backgroundColor: "#5D3FD3",
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
 }));
-
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
@@ -36,43 +36,46 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 export default function Leaderboard() {
+  const { state, getScores } = useContext(stateContext);
 
+  useEffect(() => {
+    getScores();
+  }, []);
 
-    const {state, getScores} = useContext(stateContext);
-
-    useEffect(() => {
-        getScores()
-      }, []);
-
-    if (!state.scores) return <span>loading..</span>
-    const scores = state.scores && state.scores;
-    // console.log(state);
+  if (!state.scores)
+    return (
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    );
+  const scores = state.scores && state.scores;
+  // console.log(state);
 
   return (
     <>
-      <ScoreNav/>
-    <Container sx={{mt: '6rem'}} className="container" maxWidth="lg">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>Name</StyledTableCell>
-              <StyledTableCell>Highest Earning</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {scores.map((score) => (
-              <StyledTableRow hover key={score.id}>
-                <StyledTableCell component="th" scope="row">
-                  {score.name}
-                </StyledTableCell>
-                <StyledTableCell >$ {score.score}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+      <ScoreNav />
+      <Container sx={{ mt: "6rem" }} className="container" maxWidth="lg">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Name</StyledTableCell>
+                <StyledTableCell>Highest Earning</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {scores.map((score) => (
+                <StyledTableRow hover key={score.id}>
+                  <StyledTableCell component="th" scope="row">
+                    {score.name}
+                  </StyledTableCell>
+                  <StyledTableCell>$ {score.score}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </>
   );
 }
